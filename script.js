5a392424af68c8d266a041efbb066dd43eb2dc42
Jess <jesslyndarc@gmail.com>
2023-03-07 14:53:00 +0700

    Edited

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const popupButton = document.querySelector(".profile__edit-btn");
 const closeButton = document.querySelector(".form__close-btn");
 const popupForm = document.querySelector(".form__popup");
-const formSubmit = document.querySelector(".form");
+const formElement = document.querySelector(".form");
 const profileName = document.querySelector(".profile__info_name");
 const profileJob = document.querySelector(".profile__info_bio");
-const inputName = formSubmit.querySelector(".form__name");
-const inputJob = formSubmit.querySelector(".form__bio");
+const inputName = formElement.querySelector(".form__name");
+const inputJob = formElement.querySelector(".form__bio");
 
 popupButton.addEventListener("click", openForm);
 
 closeButton.addEventListener("click", closeForm);
 
-formSubmit.addEventListener("submit", handleProfileFormSubmit);
+formElement.addEventListener("submit", handleProfileFormSubmit);
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
